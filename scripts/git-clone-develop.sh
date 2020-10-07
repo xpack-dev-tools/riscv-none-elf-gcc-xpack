81798f301c0bba7407f96ced3d1c078157355fb5
Liviu Ionescu <ilg@livius.net>
2020-10-07 12:54:36 +0300

    git-clone-develop: use xpack-develop branch

--- scripts/git-clone-develop.sh
+++ scripts/git-clone-develop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -rf "${HOME}/Downloads/riscv-none-embed-gcc-xpack.git"
-git clone --branch develop --recurse-submodules https://github.com/xpack-dev-tools/riscv-none-embed-gcc-xpack.git "${HOME}/Downloads/riscv-none-embed-gcc-xpack.git"
+git clone --branch xpack-develop --recurse-submodules https://github.com/xpack-dev-tools/riscv-none-embed-gcc-xpack.git "${HOME}/Downloads/riscv-none-embed-gcc-xpack.git"
