03616e334c5ab8ac2a9958fed477813e739e5a40
Liviu Ionescu <ilg@livius.net>
2021-11-10 08:53:49 +0200

    common-apps: increase retry 7

--- scripts/common-apps-functions-source.sh
+++ scripts/common-apps-functions-source.sh
@@ -953,7 +953,7 @@ function build_gcc_final()
               # Retry, parallel builds do fail, headers are probably
               # used before being installed. For example:
               # libstdc++-v3/include/chrono:43:10: fatal error: bits/parse_numbers.h: No such file or directory
-              for i in 1 2 3 4 5 6
+              for i in 1 2 3 4 5 6 7
               do
                 if run_verbose make -j ${JOBS} INHIBIT_LIBC_CFLAGS="-DUSE_TM_CLONE_REGISTRY=0" 
                 then
