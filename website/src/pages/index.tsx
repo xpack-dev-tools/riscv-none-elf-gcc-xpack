b9cf3adbc42d92b7778a4f7019d1a84126b4f021
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:33:24 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the GNU RISC-V Embedded GCC executables">
+      description="A binary package with the GNU RISC-V Embedded GCC executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
