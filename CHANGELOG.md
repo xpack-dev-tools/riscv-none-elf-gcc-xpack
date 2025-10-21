fc2220a6bc3c7c0b9836517b2ca6447256fbf82a
Liviu Ionescu <ilg@livius.net>
2025-10-21 16:59:18 +0300

    prepare v13.4.0-1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ Entries in this file are in reverse chronological order.
 
 ## 2025-10-21
 
+* v13.4.0-1 prepared
+* b0c6128 versioning.sh: group defs in set_multilib_list()
+* 77ff974 prepare v14.3.0-1
 * v14.3.0-1 prepared
 * c4e4c82 website updates
 * 3c357e7 package.json bump deps
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -38,7 +38,7 @@ function HomepageHeader() {
         </span>
         </p>
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/riscv-none-elf-gcc@14.3.0-1.1 --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/riscv-none-elf-gcc@13.4.0-1.1 --verbose</InstallWithCopy>
         </div>
       </div>
     </header>
